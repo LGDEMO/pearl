7328958e45aedde6ad67deb440c14891972a6f8e
ttxs <164273435>
2020-03-26 12:00:13 +0800

    严肃声明

--- gem-admin/src/main/java/com/gemframework/controller/CommController.java
+++ gem-admin/src/main/java/com/gemframework/controller/CommController.java
@@ -1,33 +1,31 @@
+/**
+ * 严肃声明：
+ * 开源版本请务必保留此注释头信息，若删除gemframe官方保留所有法律责任追究！
+ * 本软件受国家版权局知识产权以及国家计算机软件著作权保护（登记号：2018SR503328）
+ * 不得恶意分享产品源代码、二次转售等，违者必究。
+ * Copyright (c) 2020 gemframework all rights reserved.
+ * http://www.gemframework.com
+ * 版权所有，侵权必究！
+ */
 package com.gemframework.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.gemframework.common.security.configure.GemSecurityProperties;
 import com.gemframework.model.BaseResultData;
 import com.gemframework.common.enums.MenuType;
 import com.gemframework.model.vo.DeptVo;
 import com.gemframework.model.vo.MenuVo;
-import com.gemframework.model.vo.RoleVo;
-import com.gemframework.model.vo.tree.MenuSide;
 import com.gemframework.model.vo.tree.ZtreeEntity;
 import com.gemframework.service.DeptService;
 import com.gemframework.service.MenuService;
 import com.gemframework.service.RoleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-
-import static com.gemframework.common.constant.GemConstant.Auth.ROLE_PREFIX;
 
 /**
  * @Title: CommController.java
